73dc9720ec141c01371949e6d8a0089952b4d0fa
Winnie-Waiguru <winniewaiguru@gmail.com>
2022-08-23 13:18:58 +0300

    model changes

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
+from multiprocessing.connection import Client
 from django.contrib import admin
 
+from home.models import Course, Fees, Payment, Schedule, Student, Trainer, Visitor
+
 # Register your models here.
+admin.site.register(Visitor)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Schedule)
+admin.site.register(Trainer)
+admin.site.register(Fees)
+admin.site.register(Payment)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,64 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+
+class Visitor(models.Model):
+    First_name= models.CharField(max_length=50)
+    Last_name=models.CharField(max_length=50)
+    Email=models.EmailField
+    Password=models.CharField(max_length=12)
+    Age=models.SmallIntegerField()
+    Location=models.CharField(max_length=30)
+    Phone_number=models.CharField(max_length=15)
+    Interests= models.CharField(max_length= 30)
+    
+class Student(models.Model):
+    Student_image=models.ImageField()
+    Roll_number=models.IntegerField()  
+    Student_fullname=models.CharField(max_length=50, default=True)
+    Student_email=models.EmailField()
+    Student_phone_number=models.CharField(max_length=15)
+    Student_location=models.CharField(max_length=30)
+    Student_college=models.CharField(max_length=30)
+    
+class Course(models.Model):
+    Course_id=models.CharField(max_length=20, primary_key= True)
+    name=models.CharField(max_length=30)
+    duration=models.CharField(max_length= 50)
+    year=models.DateField()
+    status=models.CharField(max_length=30)
+    
+class Schedule(models.Model):
+    Schedule_id=models.CharField(max_length=40)
+    date=models.DateField()  
+    
+class Trainer(models.Model):
+    Trainer_id=models.CharField(max_length=10)
+    Full_name=models.CharField(max_length=50)
+    Trainers_picture=models.ImageField(null=True)
+    Course=models.ForeignKey('Course', on_delete=models.CASCADE,related_name='Trainer_Course')
+    Email=models.EmailField()
+    Phone_number=models.CharField(max_length=15)
+    
+class Fees(models.Model):
+    Fee_id=models.CharField(max_length=10) 
+    Amount=models.PositiveIntegerField()
+    Balance=models.PositiveIntegerField()
+    Status=models.CharField(max_length=40)
+    Course=models.ForeignKey('Course', on_delete=models.CASCADE,related_name='Fees_Course') 
+
+class Payment(models.Model):
+    Payment_id=models.CharField(max_length=10)
+    Student_roll_number=models.ForeignKey('Student',on_delete=models.CASCADE,related_name='Student_Payment')
+    Amount=models.ForeignKey('Fees',on_delete=models.CASCADE,related_name='Course_Amount')
+    Status=models.CharField(max_length=40)
+    Payment_date=models.DateField()
+    Due_date=models.DateTimeField()
+    
+          
+      
+      
+    
+        
+    
